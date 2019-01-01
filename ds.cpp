1962e36287f6be34376559f49a3abcca1b17317d
ytwang <ytwang3579@gmail.com>
2019-01-01 15:34:37 +0800

    random

--- ds.cpp
+++ ds.cpp
@@ -26,30 +26,3 @@ class Student{
         int x;
         int y;
 };
-
-class TreeNode{
-    public:
-        TreeNode(){
-            
-        }
-    private:
-        TreeNode* parent;
-        vector<TreeNode*> child;
-        int x, y, value;
-}
-
-class GameTree{
-    public:
-        void simulate(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor){
-            for(int i=0; i<5; i++){
-                for(int j=0; j<6; j++){
-                    if(color[i][j]==White || color[i][j]==inputColor){
-                        
-                    }
-                }
-            }
-        }
-        
-    private:
-        vector<pair<int, int>> value;
-};
