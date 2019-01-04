36acde6cf2135197c13c0045287ca45354cf02eb
ytwang <ytwang3579@gmail.com>
2019-01-05 02:19:13 +0800

    Tree Node

--- dst.cpp
+++ dst.cpp
@@ -1,28 +1,3 @@
-    #include <bits/stdc++.h>
-    //#define DEBUG_MODE
-    //#define SIM_MODE
-    #define CHECK
-    #define DEBUG_MAIN
-    
-    using namespace std;
-
-    enum PlayStatus{
-        // not important
-        Terminate,
-        Done,
-        Continue
-    };
-
-    enum Color{
-        White, // Initital 0
-        Blue, // Player One
-        Red, // Player Two
-        Black //Explosion
-    };
-
-    namespace playerOne
-    {
-        // Your Code
 class Tree;
 class Node{
     public:
@@ -554,8 +529,8 @@ class Tree{
                 root->child.pop_back();
             }
             if(tmpv==999){
-                cout << "Pneumonoultramicroscopicsilicovolcanoconiosis\n";
-                cout << initx+1 << ' ' << inity+1 << '\n';
+                //cout << "Pneumonoultramicroscopicsilicovolcanoconiosis\n";
+                //cout << initx+1 << ' ' << inity+1 << '\n';
                 tmpv--;
             }
             root->value[initx][inity] = tmpv;
@@ -584,6 +559,14 @@ class Student{
     public:
         void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor){
             // Your Code
+            for(int i=0; i<5; i++){
+                for(int j=0; j<6; j++){
+                    if(Record[i][j]==Max[i][j]) Record[i][j] = -1;
+                    cout << Record[i][j] << ' ';
+                }
+                cout << '\n';
+            }
+
             tree = new Tree(Record, color, inputColor);
             tree->Grow();
             
@@ -593,219 +576,30 @@ class Student{
                 }
                 cout << '\n';
             }
-            
+
+            if(inputColor==Blue){
+                int i, j;
+                i = x%5; j = y%6;
+                cout << "Player One : (" << i << ',' << j << ")\n";
+            } else {
+                int i, j;
+                i = x%5; j = y%6;
+                cout << "Player Two : (" << i << ',' << j << ")\n";
+            }
         }
         // Any Code You Want to Add
         int getX(){
             // Your Code
-            return tree->GetBestX();
+            x = tree->GetBestX();
+            return x;
         }
         int getY(){
             // Your Code
-            return tree->GetBestY();
+            y = tree->GetBestY();
+            return y;
         }
     private:
         int x;
         int y;
         Tree* tree;
 };
-    };
-
-    namespace playerTwo
-    {
-        // Your Code
-        class Student{
-            public:
-                void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor){
-                    // Your Code
-                    clock_t t;
-                    int tmpi, tmpj;
-                    do{
-                        t = clock();
-                        tmpi = t % 5;
-                        t = clock();
-                        tmpj = t % 6;
-                    } while(color[tmpi][tmpj]!=White && color[tmpi][tmpj]!=inputColor);
-#ifndef CHECK
-                    x = tmpi;
-                    y = tmpj;
-#endif
-#ifdef CHECK
-                    cin >> x >> y;
-                    x--; y--;
-#endif
-                }
-                // Any Code You Want to Add
-                int getX(){
-                    // Your Code
-                    return x;
-                }
-                int getY(){
-                    // Your Code
-                    return y;
-                }
-            private:
-                int x;
-                int y;
-        };
-    };
-
-    int main()
-    {
-        // something check who is the winner
-        int Record[5][6] = {{0}};
-        int Max[5][6] = {{2,3,3,3,3,2}, {3,4,4,4,4,3}, {3,4,4,4,4,3}, {3,4,4,4,4,3}, {2,3,3,3,3,2}};
-        Color color[5][6] = {{White}};
-        PlayStatus status = Done;
-        playerTwo::Student student1;
-        playerOne::Student student2;
-        int x, y;
-        cout << "Start\n";
-
-        while(status){
-            student1.makeMove(Record, Max, color, Blue);
-            x = student1.getX(); y = student1.getY();
-            if(color[x][y]==Blue || color[x][y]==White) {
-#ifndef SIM_MODE
-                cout << "Player One : (" << x+1 << "," << y+1 << ")\n";
-#endif
-                Record[x][y]++; color[x][y] = Blue;
-                if(Record[x][y]==Max[x][y]) status = Continue;
-            } else {
-                cout << "Player Two Win !\n"; break;
-            }
-            
-            while(status==Continue){
-                Record[x][y] = -1; color[x][y] = Black;
-
-                if(y>0){
-                    if(color[x][y-1] != Black) {
-                        Record[x][y-1]++; color[x][y-1] = Blue;
-                    }
-                }
-                if(y<5){
-                    if(color[x][y+1] != Black) {
-                        Record[x][y+1]++; color[x][y+1] = Blue;
-                    }
-                }
-                if(x>0){
-                    if(color[x-1][y] != Black) {
-                        Record[x-1][y]++; color[x-1][y] = Blue;
-                    }
-                }
-                if(x<4){
-                    if(color[x+1][y] != Black) {
-                        Record[x+1][y]++; color[x+1][y] = Blue;
-                    }
-                }
-
-                status = Terminate;
-                for(x=0; x<5; x++){
-                    for(y=0; y<6; y++){
-                        if(Record[x][y]>=Max[x][y]){
-                            status = Continue; break;
-                        }
-                    }
-                    if(status) break;
-                }
-            }
-
-#ifdef DEBUG_MAIN
-                
-                cout << "Record : \n";
-                for(int i=0; i<5; i++){
-                    for(int j=0; j<6; j++){
-                        cout << Record[i][j] << ' ';
-                    }
-                    cout << '\n';
-                }
-                cout << "x : " << x+1 << " y : " << y+1 << '\n';
-#endif
-
-            for(x=0; x<5; x++){
-                for(y=0; y<6; y++){
-                    if(color[x][y]==White || color[x][y]==Red){
-                        status = Done; break;
-                    }
-                }
-                if(status) break;
-            }
-            if(status==Terminate){
-                cout << "Player One Win !\n"; break;
-            }
-            
-            student2.makeMove(Record, Max, color, Red);
-            x = student2.getX(); y = student2.getY();
-            if(color[x][y]==Red || color[x][y]==White) {
-#ifndef SIM_MODE
-                cout << "Player Two : (" << x+1 << "," << y+1 << ")\n";
-#endif
-                Record[x][y]++; color[x][y] = Red;
-                if(Record[x][y]==Max[x][y]) status = Continue;
-            } else {
-                cout << "Player One Win !\n"; break;
-            }
-            
-            while(status==Continue){
-                Record[x][y] = -1; color[x][y] = Black;
-
-                if(y>0){
-                    if(color[x][y-1] != Black) {
-                        Record[x][y-1]++; color[x][y-1] = Red;
-                    }
-                }
-                if(y<5){
-                    if(color[x][y+1] != Black) {
-                        Record[x][y+1]++; color[x][y+1] = Red;
-                    }
-                }
-                if(x>0){
-                    if(color[x-1][y] != Black) {
-                        Record[x-1][y]++; color[x-1][y] = Red;
-                    }
-                }
-                if(x<6){
-                    if(color[x+1][y] != Black) {
-                        Record[x+1][y]++; color[x+1][y] = Red;
-                    }
-                }
-
-                status = Terminate;
-                for(x=0; x<5; x++){
-                    for(y=0; y<6; y++){
-                        if(Record[x][y]>=Max[x][y]){
-                            status = Continue; break;
-                        }
-                    }
-                    if(status) break;
-                };
-                
-            }
-
-#ifdef DEBUG_MAIN
-                
-                cout << "Record : \n";
-                for(int i=0; i<5; i++){
-                    for(int j=0; j<6; j++){
-                        cout << Record[i][j] << ' ';
-                    }
-                    cout << '\n';
-                }
-                cout << "x : " << x+1 << " y : " << y+1 << '\n';
-#endif
-
-            for(x=0; x<5; x++){
-                for(y=0; y<6; y++){
-                    if(color[x][y]==White || color[x][y]==Blue){
-                        status = Done; break;
-                    }
-                }
-                if(status) break;
-            }
-            if(status==Terminate){
-                cout << "Player Two Win !\n"; break;
-            }
-        }
-        
-        return 0;
-    }
